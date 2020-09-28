29b00fb0f386912174d0e0a65310c466061e38c3
bran9230 <branjay18@yahoo.com>
2020-09-28 18:41:29 -0400

    RandomQuoteGen - Updated w/ Project Req.
    Issue with printQuote() method.

--- Random Quote Generator.js
+++ Random Quote Generator.js
@@ -1,30 +1,81 @@
 
 //Create a vairable that will hold the quote and the Author of the quote
 
-const quote = [
-    "The best way to remember your wife's birthday is to forget it once. - Bertrand Russell",
-    "Science is what you know, philosophy is what you don't know. - E. Joseph Cossman", 
-    "Anything that comes easy, comes wrong. - Josephine Tessier"   
-];
+
+// Create an Array of Objects to store in the data for your quotes
+// Add data to your quote objects
+const quotes = [
+    {
+        quote: "The best way to remember your wife's birthday is to forget it once.",
+        source: "Bertrand Russell",
+        citation: "Google",
+        year: ''
+    },
+    {
+        quote: "Science is what you know, philosophy is what you don't know.",
+        source: "E. Joseph Cossman",
+        citation: "Google",
+        year: ''
+    },
+    {
+        quote: "Anything that comes easy, comes wrong.",
+        source: "Josephine Tessier",
+        citation: "Google",
+        year: ''
+    },
+    {
+        quote: "Do it, just do it. Don't let your dreams be dreams.",
+        source: "Shia LaBeouf",
+        citation: "Google",
+        year: ''
+    },
+    {
+        quote: "Make your life a masterpiece; imagine no limitations on what you can be, have or do",
+        source: "Brian Tracy",
+        citation: "Google",
+        year: ''
+    },
+]
 
 
-
-// Need a Loop to iterate over quotes
-function getRandomQuote(arr) {
-    let html = '';
-    for (let i = 0; i < arr.length; i++) {
-        let random = Math.floor(Math.random() * (arr.length) + 1);
-        html += `${arr[i]}`;
+/* 
+Create the getRandomQuote function
+The getRandomQuote function should create a random number,
+and use that random number to return a random quote object from the quotes array.
+*/
+    function getRandomQuote() {
+        let random = Math.floor(Math.random() * (quotes.length));
+        return quotes[random];
+        //console.log(quotes[random]);
+        // document.getElementById('html').innerHTML = quotes[random]; 
     }
-        //console.log(html);
-        return html;
-}
-document.getElementById("html").innerHTML = getRandomQuote(quote);
+
+    // getRandomQuote();
+    // console.log(getRandomQuote());
+
+// Create a printQuote Function
+// BRANDON -- PICK UP HERE 8/28
+
+function printQuote() {
+    let result = getRandomQuote();
     
+    let randomQuote = `
+        <p class = "quote">${result.quote}</p> 
+        <p class = "source">${result.source}
+    `
 
+    if(result.citation || result.year) {
+        randomQuote = `
+        <span class = "citation">${result.citation}</span>
+        <span class = "year">${result.year}</span>
+        </p>
+    `
+    }
 
+    document.getElementById('html').innerHTML = randomQuote;
+    console.log(randomQuote);
 
+}
 
-
-// Need to display the quotes in the <div class = "Container-Card"></div>
+// printQuote();
 
